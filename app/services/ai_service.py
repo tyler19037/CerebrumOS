9d07250001a348d4a584ee89a942bd29a84ef7a5
tyler19037 <tcr4@email.sc.edu>
2026-07-06 23:18:03 -0400

    Established basic chatting with AI

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,2 +1,16 @@
+import google.generativeai as genai
+from app.core.config import GEMINI_API_KEY
+
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
+)
+
+
 def generate_response(message: str) -> str:
-    return f"TitanOS received your message: {message}"
+    response = model.generate_content(message)
+
+    return response.text
